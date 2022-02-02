f69ac78e73a90a1915ff2a19fe01679d236d17c0
Martín <magadiflo@gmail.com>
2022-02-02 18:43:44 -0500

    Logout o cerrar sesión

---
+++ src/main/java/org/magadiflo/webapp/jsf3/controllers/LogoutController.java
@@ -0,0 +1,25 @@
+package org.magadiflo.webapp.jsf3.controllers;
+
+import jakarta.enterprise.inject.Model;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Model
+public class LogoutController {
+
+    @Inject
+    private FacesContext facesContext;
+
+    public String logout() throws ServletException {
+        HttpServletRequest request = (HttpServletRequest) this.facesContext.getExternalContext().getRequest();
+        request.logout();
+        request.getSession().invalidate();
+
+        this.facesContext.addMessage(null, new FacesMessage("Haz cerrado sesión con éxito!"));
+        return "login.xhtml";
+    }
+
+}
